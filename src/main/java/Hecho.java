18dd4c69a0036abe73bb687541cb7d6fa5b13520
regueiranehuen <nehuenregueira@gmail.com>
2025-04-17 16:30:55 -0300

    inicio a codear

---
+++ src/main/java/Hecho.java
@@ -0,0 +1,16 @@
+import java.util.Date;
+
+public class Hecho {
+
+    private String titulo;
+    private String descripcion;
+    private Categoria categoria;
+    private ContenidoMultimedia contenidoMultimediaOpcional;
+    private Pais pais;
+    private Date fechaAcontecimiento;
+    private Date fechaDeCarga;
+    private Origen origen;
+
+
+
+}
